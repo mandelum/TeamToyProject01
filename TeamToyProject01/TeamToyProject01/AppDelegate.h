e84ce89acb23de0e4d9395943e74623796a34336
Robin Brandt <mandelum@gmail.com>
2012-02-07 12:14:59 +0100

    kjb

---
+++ TeamToyProject01/TeamToyProject01/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TeamToyProject01
+//
+//  Created by Robin Brandt on 2012-02-07.
+//  Copyright (c) 2012 Hypno Abstract. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TeamToyProject01/TeamToyProject01/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TeamToyProject01
+//
+//  Created by Robin Brandt on 2012-02-07.
+//  Copyright (c) 2012 Hypno Abstract. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
